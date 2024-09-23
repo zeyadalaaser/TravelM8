8e6507759d916b4b55dafa754d601bef266ae4c2
zidanesalma <zidanesalma080@gmail.com>
2024-09-23 20:48:00 +0300

    starter code

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+
+import { connectDB } from "./config/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
+});
