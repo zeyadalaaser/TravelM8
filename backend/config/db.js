1618db36243a4fd2b6825cfe1a8c0ddc73551229
zidanesalma <zidanesalma080@gmail.com>
2024-09-23 23:10:09 +0300

    fixed .env path

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config({path:'../../.env'});
 
 export const connectDB = async () => {
-	// console.log("MongoDB URI:", process.env.MONGO_URI);
+	console.log("MongoDB URI:", process.env.MONGO_URI);
 	try {
-		const conn = await mongoose.connect("mongodb+srv://team:Team1234@cluster0.befui0o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+		const conn = await mongoose.connect(process.env.MONGO_URI);
 		console.log("MongoDB Connection successful!");
 	} catch (error) {
 		console.log("Connection Failed:", error);
