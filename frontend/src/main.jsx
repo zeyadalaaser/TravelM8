f62a2e9d6c46e19d6869d7abd5add41c401ff0a2
zeina <walidzeina1@gmail.com>
2024-10-07 00:54:12 +0300

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 import TouristPage from '@/pages/tourist/tourist-page.jsx';
-import TourGuide from '@/pages/TourGuide/tourguide.jsx'
 import Login from '@/pages/SignIn/Login.jsx'; 
 import SignupGeneral from '@/pages/SignUp/SignupGeneral.jsx';
 import TouristRegistration from '@/pages/SignUp/signupTourist.jsx'
@@ -32,10 +31,15 @@ createRoot(document.getElementById('root')).render(
         <Route path="/deleteUser" element={<DeleteUser/>} />
         <Route path="/preferenceTag" element={<PreferenceTag/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
-
         <Route path="/admin/addAdmin" element={<Admin />} />
-          <Route path="/admin/EditActivityCategories" element={<ActivityCategories />} />
-          <Route path="/admin/addTourismGovernor" element={<TourismGovernor />} />
+        <Route path="/admin/EditActivityCategories" element={<ActivityCategories />} />
+        <Route path="/admin/addTourismGovernor" element={<TourismGovernor />} />
+        <Route path="login" element={<Login />} />
+        <Route path="/signup" element={<SignupGeneral />} />
+        <Route path="/signup/signupTourist" element={<TouristRegistration />} />
+        <Route path="/signup/signupTourguide" element={<FormPage />} />
+        <Route path="/signup/signupSeller" element={<FormPageSeller />} />
+        <Route path="/tourist-page" element={<TouristPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
