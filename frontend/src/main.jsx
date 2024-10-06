b950d9673acad36e64f99c9de514aa4a135453b5
Arwataha1 <arwataha1@gmail.com>
2024-10-06 19:31:00 +0300

    modify imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-//import TouristPage from '@/pages/tourist/tourist-page.jsx';
-import SignUpForm from './signupTourist.jsx'
-//import ProfileTemplate from './profileTemplate.jsx'
+import SignUpForm from '@/pages/SignUp/signupTourist.jsx'
 // import AdminPage from '@/pages/admin/admin-page.jsx'
 import ProfileTemplate from '@/pages/TourGuide/profileTemplate.jsx'
-import ItineraryManager from '/Users/arwataha/Documents/GitHub/TravelM8/frontend/src/pages/TourGuide/itinerary.jsx';
-//import AdminPage from '@/pages/admin/admin-page.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
 
@@ -15,8 +11,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<AdminPage/>} />
+        <Route path="/" element={<ProfileTemplate />} />
+        {/* <Route path="/admin" element={<AdminPage/>} /> */}
         <Route path="/sign-up-tourist" element={<SignUpForm/>} />
       </Routes>
     </BrowserRouter>
