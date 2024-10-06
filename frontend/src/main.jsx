aa8a2c53df837ca549d30ffb6e09e2c7cae3f11b
zeina <walidzeina1@gmail.com>
2024-10-06 21:58:46 +0300

    dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,9 @@ import App from './App.jsx'
 //import TouristPage from '@/pages/tourist/tourist-page.jsx';
 //import SignUpForm from './signupTourist.jsx'
 //import ProfileTemplate from './profileTemplate.jsx'
-import AdminPage from '@/pages/admin/admin-page.jsx'
+//import AdminDashboard from './pages/admin/dashboard.jsx'
+//import DeleteUser from './pages/admin/deleteUser-page.jsx';
+import PreferenceTag from '../../backend/models/preferenceTagModel.js';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
 
@@ -13,7 +15,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/admin" element={<AdminPage/>} />
+        <Route path="/preferenceTag" element={<PreferenceTag/>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
