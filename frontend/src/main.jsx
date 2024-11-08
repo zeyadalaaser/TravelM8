e562d7b8cc404fc77dec210a1b5553e58d0e2904
NouranAyman56 <nourankamha@gmail.com>
2024-11-08 14:38:28 +0200

    routes fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -24,17 +24,13 @@ import AdvertiserRegistration from "@/pages/SignUp/signupAdvertiser.jsx";
 import SellerProfile from "@/pages/seller/SellerProfile.jsx";
 import SellerProducts from "@/pages/seller/SellerProducts.jsx";
 import ComplaintsPage from "@/pages/admin/complaints.jsx";
-
-import { ProductsPage } from "./pages/tourist/components/products/products-page.jsx";
-import PurchasedProductsPage from "@/pages/tourist/components/products/PurchasedProductsPage.jsx";
 import AdminItinerariesPage from "./pages/admin/AdminItineraryPage.jsx";
 import TourGuideDashboard from "./pages/TourGuide/TourGuideDashboard.jsx";
-
 import ItineraryForm from "./pages/TourGuide/ItineraryForm.jsx";
 import ActivityForm from "./pages/Advertiser/ActivityForm.jsx";
 import AdvertiserDashboard from "./pages/Advertiser/advertiserDashboard.jsx";
-import { ProductsPage } from "./pages/tourist/components/products/products-page.jsx";
-import PurchasedProductsPage from "@/pages/tourist/components/products/PurchasedProductsPage.jsx";
+//import { ProductsPage } from "./pages/tourist/components/products/products-page.jsx";
+//import PurchasedProductsPage from "@/pages/tourist/components/products/PurchasedProductsPage.jsx";
 import PendingUserDocuments from "./pages/admin/PendingUsersDocuments.jsx";
 import Sellerdashboard from '@/pages/seller/Sellerdashboard.jsx';
 import AccountDelete from "./pages/admin/accountDelete.jsx"
