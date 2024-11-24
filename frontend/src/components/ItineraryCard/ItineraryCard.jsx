c7bbacc7972a0cb7109d9a89b391dd998c7018d7
Zeyad <93214240+zeyadalaaser@users.noreply.github.com>
2024-11-24 22:20:30 +0200

    update styling

--- frontend/src/components/ItineraryCard/ItineraryCard.jsx
+++ frontend/src/components/ItineraryCard/ItineraryCard.jsx
@@ -38,7 +38,7 @@ export default function ItineraryCard({
   onRefresh,
   isTourGuide,
 }) {
-   
+
   const navigate = useNavigate();
 
 
@@ -52,8 +52,8 @@ export default function ItineraryCard({
       );
       if (!response.ok) {
         const data = await response.json();
-        alert(data.message); 
-        return;       
+        alert(data.message);
+        return;
       }
       await onRefresh();
       alert("Itinerary Deleted successfully");
@@ -64,7 +64,7 @@ export default function ItineraryCard({
   };
 
   const handleActivationToggle = async (id, isBookingOpen) => {
-    const state = isBookingOpen?"Deactivated":"Activated";
+    const state = isBookingOpen ? "Deactivated" : "Activated";
     try {
       const response = await fetch(`http://localhost:5001/api/itineraries/${id}`,
         {
@@ -77,8 +77,8 @@ export default function ItineraryCard({
       );
       if (!response.ok) {
         const data = await response.json();
-        alert(data.message); 
-        return;       
+        alert(data.message);
+        return;
       }
       onRefresh();
       alert(`Itinerary ${state} successfully`);
@@ -89,7 +89,7 @@ export default function ItineraryCard({
   }
 
   // const handleBook = async (itineraryId, tourGuideId) => {
-    
+
 
   // }; 
 
@@ -109,151 +109,163 @@ export default function ItineraryCard({
         {itineraries?.map((itinerary) => (
           <Card key={itinerary._id}>
             <div className="flex flex-row">
-              <div className="flex-1 w-full md:w-2/3 p-4">
-                <div className="flex justify-between">
-                  <h3 className="text-xl font-semibold mb-2">
-                    {itinerary.name}
-                  </h3>
-                  <div className="flex items-center gap-2 ">
-                    {isTourGuide && itinerary.isBookingOpen && (
-                      <Button
-                        className="w-[150px] h-[38px] bg-sky-800 hover:bg-sky-900"
-                        onClick={() =>
-                          handleActivationToggle(
-                            itinerary._id,
-                            itinerary.isBookingOpen
-                          )
-                        }
-                      >
-                        Deactivate Itinerary
-                      </Button>
-                    )}
-                    {isTourGuide && !itinerary.isBookingOpen && (
-                      <Button
-                        className="w-[150px] h-[38px] bg-sky-500 hover:bg-sky-600"
-                        onClick={() =>
-                          handleActivationToggle(itinerary._id, false)
-                        }
-                      >
-                        Activate Itinerary
-                      </Button>
-                    )}
-                  {isTourist && <ShareButton id={itinerary._id} name="itinerary" />}
-                  </div>
-                </div>
-                <div className="flex items-center mb-2">
-                  <Stars rating={itinerary.averageRating} />
-                  <span className="ml-2 text-sm text-gray-600">
-                    {itinerary.totalRatings} reviews
-                  </span>
-                </div>
-                <p className="text-sm text-gray-600 mb-2">
-                  {itinerary.description}
-                </p>
-                {itinerary.activities.length > 0 && (
-                  <div className="flex items-center text-sm text-gray-600 mb-2 gap-2">
-                    <Label className="text-m font-semibold text-black">
-                      Activities:
-                    </Label>
-                    <div className="flex items-center gap-1">
-                      {itinerary.activities.map((activity, index) => (
-                        <div
-                          key={index}
-                          className="flex items-center text-sm text-gray-600"
+              <div className="w-1/3">
+                <img
+                  src={
+                    itinerary.images[0] ||
+                    "/placeholder.svg?height=200&width=300"
+                  }
+                  alt={itinerary.name}
+                  className="w-full min-h-full"
+                />
+              </div>
+              <div className="flex flex-row w-2/3">
+                <div className="flex-1 w-full p-4">
+                  <div className="flex justify-between">
+                    <h3 className="text-xl font-semibold mb-2">
+                      {itinerary.name}
+                    </h3>
+                    <div className="flex items-center gap-2 ">
+                      {isTourGuide && itinerary.isBookingOpen && (
+                        <Button
+                          className="w-[150px] h-[38px] bg-sky-800 hover:bg-sky-900"
+                          onClick={() =>
+                            handleActivationToggle(
+                              itinerary._id,
+                              itinerary.isBookingOpen
+                            )
+                          }
                         >
-                          {activity} -
-                        </div>
-                      ))}
+                          Deactivate Itinerary
+                        </Button>
+                      )}
+                      {isTourGuide && !itinerary.isBookingOpen && (
+                        <Button
+                          className="w-[150px] h-[38px] bg-sky-500 hover:bg-sky-600"
+                          onClick={() =>
+                            handleActivationToggle(itinerary._id, false)
+                          }
+                        >
+                          Activate Itinerary
+                        </Button>
+                      )}
+                      {isTourist && <ShareButton id={itinerary._id} name="itinerary" />}
                     </div>
                   </div>
-                )}
-                {itinerary.historicalSites.length > 0 && (
-                  <div className="flex items-center text-sm text-gray-600 mb-2 gap-2">
-                    <Label className="text-m font-semibold text-black">
-                      Sites:
-                    </Label>
-                    <div className="flex items-center gap-1">
-                      {itinerary.historicalSites.map((site, index) => (
-                        <div
-                          key={index}
-                          className="flex items-center text-sm text-gray-600"
-                        >
-                          {site} -
-                        </div>
-                      ))}
-                    </div>
+                  <div className="flex items-center mb-2">
+                    <Stars rating={itinerary.averageRating} />
+                    <span className="ml-2 text-sm text-gray-600">
+                      {itinerary.totalRatings} reviews
+                    </span>
                   </div>
-                )}
-                <div className="flex items-center text-sm text-gray-600 mb-2 gap-2">
-                  <Label className="text-m font-semibold text-black">
-                    Available Slots:
-                  </Label>
-                  <div className="flex items-center gap-4">
-                    {itinerary.availableSlots.map((slot, index) => (
-                      <div
-                        key={index}
-                        className="flex items-center text-sm text-gray-600"
-                      >
-                        <Clock className="w-4 h-4 mr-1" />
-                        <span>{slot.date.slice(0, 10)}</span>
+                  <p className="text-sm text-gray-600 mb-2">
+                    {itinerary.description}
+                  </p>
+                  {itinerary.activities.length > 0 && (
+                    <div className="flex items-center text-sm text-gray-600 mb-2 gap-2">
+                      <Label className="text-m font-semibold text-black">
+                        Activities:
+                      </Label>
+                      <div className="flex items-center gap-1">
+                        {itinerary.activities.map((activity, index) => (
+                          <div
+                            key={index}
+                            className="flex items-center text-sm text-gray-600"
+                          >
+                            {activity} -
+                          </div>
+                        ))}
                       </div>
+                    </div>
+                  )}
+                  {itinerary.historicalSites.length > 0 && (
+                    <div className="flex items-center text-sm text-gray-600 mb-2 gap-2">
+                      <Label className="text-m font-semibold text-black">
+                        Sites:
+                      </Label>
+                      <div className="flex items-center gap-1">
+                        {itinerary.historicalSites.map((site, index) => (
+                          <div
+                            key={index}
+                            className="flex items-center text-sm text-gray-600"
+                          >
+                            {site} -
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                  <div className="flex items-center text-sm text-gray-600 mb-2 gap-2">
+                    <Label className="text-m font-semibold text-black">
+                      Available Slots:
+                    </Label>
+                    <div className="flex items-center gap-4">
+                      {itinerary.availableSlots.map((slot, index) => (
+                        <div
+                          key={index}
+                          className="flex items-center text-sm text-gray-600"
+                        >
+                          <Clock className="w-4 h-4 mr-1" />
+                          <span>{slot.date.slice(0, 10)}</span>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="flex items-center flex-wrap gap-2 mb-2">
+                    <Globe className="w-4 h-4 mr-1" />
+                    <Badge variant="secondary">{itinerary.tourLanguage}</Badge>
+                  </div>
+                  <div className="flex items-center flex-wrap gap-2 mb-2">
+                    <Tag className="w-4 h-4 mr-1" />
+                    {itinerary.tags.map((tag, tagIndex) => (
+                      <Badge key={tagIndex} variant="secondary">
+                        {tag?.name ?? tag}
+                      </Badge>
                     ))}
                   </div>
-                </div>
-                <div className="flex items-center flex-wrap gap-2 mb-2">
-                  <Globe className="w-4 h-4 mr-1" />
-                  <Badge variant="secondary">{itinerary.tourLanguage}</Badge>
-                </div>
-                <div className="flex items-center flex-wrap gap-2 mb-2">
-                  <Tag className="w-4 h-4 mr-1" />
-                  {itinerary.tags.map((tag, tagIndex) => (
-                    <Badge key={tagIndex} variant="secondary">
-                      {tag?.name ?? tag}
-                    </Badge>
-                  ))}
-                </div>
-                <div className="flex justify-end items-center space-x-2">
-                  <span className="text-xl font-bold mr-auto">{`${(itinerary.price * 1).toFixed(2)} ${currency}`}</span>
-                  <Timeline selectedItinerary={itinerary}/>
-                  {isTourist && (
-                    <div className="flex justify-end items-center">
-                      {/* <Button onClick={modalOpen(true)}>
+                  <div className="flex justify-end items-center space-x-2">
+                    <span className="text-xl font-bold mr-auto">{`${(itinerary.price * 1).toFixed(2)} ${currency}`}</span>
+                    <Timeline selectedItinerary={itinerary} />
+                    {isTourist && (
+                      <div className="flex justify-end items-center">
+                        {/* <Button onClick={modalOpen(true)}>
                             Book Activity!
                           </Button> */}
-                      <ChooseDate itinerary={itinerary} />
-                    </div>
-                  )}
-                  {isTourGuide && (
-                    <div className="flex items-center gap-2">
+                        <ChooseDate itinerary={itinerary} />
+                      </div>
+                    )}
+                    {isTourGuide && (
+                      <div className="flex items-center gap-2">
+                        <Button
+                          className="hover:bg-red-700"
+                          onClick={() => handleDelete(itinerary._id)}
+                          variant="destructive"
+                        >
+                          Delete
+                        </Button>
+                        <Button
+                          variant="secondary"
+                          className="hover:bg-gray-300"
+                          onClick={() =>
+                            navigate("/itineraryForm", {
+                              state: { itinerary: itinerary },
+                            })
+                          }
+                        >
+                          Update
+                        </Button>
+                      </div>
+                    )}
+                    {isAdmin && (
                       <Button
                         className="hover:bg-red-700"
-                        onClick={() => handleDelete(itinerary._id)}
                         variant="destructive"
+                        onClick={() => handleFlagItinerary(itinerary._id)}
                       >
-                        Delete
+                        Flag as Inappropriate
                       </Button>
-                      <Button
-                        variant="secondary"
-                        className="hover:bg-gray-300"
-                        onClick={() =>
-                          navigate("/itineraryForm", {
-                            state: { itinerary: itinerary },
-                          })
-                        }
-                      >
-                        Update
-                      </Button>
-                    </div>
-                  )}
-                  {isAdmin && (
-                    <Button
-                      className="hover:bg-red-700"
-                      variant="destructive"
-                      onClick={() => handleFlagItinerary(itinerary._id)}
-                    >
-                      Flag as Inappropriate
-                    </Button>
-                  )}
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
@@ -265,34 +277,34 @@ export default function ItineraryCard({
 }
 
 
-const Timeline = ({selectedItinerary}) => {
+const Timeline = ({ selectedItinerary }) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="outline">View Timeline</Button>
       </DialogTrigger>
-        <DialogContent>
-          <DialogTitle>Itinerary Timeline</DialogTitle>
-          <div className="flex flex-col mt-4">
-            {selectedItinerary?.timeline.map((event, index) => (
-              <div key={index} className="flex items-center mb-2">
-                <div className="w-4 h-4 bg-black rounded-full mr-2"></div>{" "}
-                {/* Dot */}
-                <div className="flex-1">
-                  <h4 className="font-semibold">{event.event}</h4>{" "}
-                  {/* Adjust based on your data structure */}
-                  <p>Start: {new Date(event.startTime).toLocaleString()}</p>
-                  <p>End: {new Date(event.endTime).toLocaleString()}</p>
-                </div>
+      <DialogContent>
+        <DialogTitle>Itinerary Timeline</DialogTitle>
+        <div className="flex flex-col mt-4">
+          {selectedItinerary?.timeline.map((event, index) => (
+            <div key={index} className="flex items-center mb-2">
+              <div className="w-4 h-4 bg-black rounded-full mr-2"></div>{" "}
+              {/* Dot */}
+              <div className="flex-1">
+                <h4 className="font-semibold">{event.event}</h4>{" "}
+                {/* Adjust based on your data structure */}
+                <p>Start: {new Date(event.startTime).toLocaleString()}</p>
+                <p>End: {new Date(event.endTime).toLocaleString()}</p>
               </div>
-            ))}
-          </div>
-        </DialogContent>
-      </Dialog>
+            </div>
+          ))}
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
 
-const ChooseDate = ({itinerary}) => {
+const ChooseDate = ({ itinerary }) => {
   let remainingSpots;
   const [selectedDate, setSelectedDate] = useState();
   const [submitStatus, setSubmitStatus] = useState();
