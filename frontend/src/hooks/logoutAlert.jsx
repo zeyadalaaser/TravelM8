5afa0d0fa0af16680aa85c6e425220bd635b2328
Zeyad <93214240+zeyadalaaser@users.noreply.github.com>
2024-12-04 05:31:44 +0200

    logout

--- frontend/src/hooks/logoutAlert.jsx
+++ frontend/src/hooks/logoutAlert.jsx
@@ -3,28 +3,24 @@ import React from "react";
 import { useNavigate } from 'react-router-dom';
 import { Button } from "@/components/ui/button.jsx"; // Adjust the import path accordingly
 
-const LogoutAlertDialog = ({ isOpen, onClose, onLogout}) => {
+const LogoutAlertDialog = ({ isOpen, onClose, onLogout }) => {
   if (!isOpen) return null; // Don't render the dialog if it's not open
   const navigate = useNavigate();
 
   const handleLogout = async () => {
     const token = localStorage.getItem('token');
     try {
-      const response = await fetch('http://localhost:5001/api/logout', {
+      await fetch('http://localhost:5001/api/logout', {
         method: 'POST',
         headers: {
           'Authorization': `Bearer ${token}`,
           'Content-Type': 'application/json'
         }
       });
-       
-      if (response.ok) {
-        localStorage.removeItem('token'); 
-        onLogout();
-        navigate("/"); 
-      } else {
-        console.error('Failed to logout');
-      }
+
+      localStorage.removeItem('token');
+      onLogout?.();
+      navigate("/");
     } catch (error) {
       console.error('Error logging out:', error);
     }
@@ -38,7 +34,7 @@ const LogoutAlertDialog = ({ isOpen, onClose, onLogout}) => {
           <Button
             onClick={() => {
               handleLogout();
-              onClose(); 
+              onClose();
             }}
             className="bg-red-500 text-white border-none focus:outline-none hover:bg-red-500"
           >
